3775611d04bb3a449ed718a21526f818861eeeb3
thomas.convain <thomas.convain@gmail.com>
2024-08-24 15:05:40 -0400

    set firebase tools

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLgwoTQ1EB59nr9rerF0gonBbblZ9IJ0g",
+  authDomain: "minutos-87fe9.firebaseapp.com",
+  projectId: "minutos-87fe9",
+  storageBucket: "minutos-87fe9.appspot.com",
+  messagingSenderId: "28186647799",
+  appId: "1:28186647799:web:1228819cff4f9d77c987f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
