96fd36f25063e4bdee5ab806724dff88e41895a5
thomas.convain <thomas.convain@gmail.com>
2024-09-28 21:26:18 +0200

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,7 @@ const routes = [
   },
   { path: '/event/:idSpectator/:idEvent/:nameEvent', 
     name: 'EventDetail', 
-    component: EventDetail 
+    component: EventDetail,
   },
   {
     path: '/checkout/:idSpectator/:idEvent/:nameEvent?',
