08c43fcb6c53d553bf347e25c06952fea60f6d22
thomas.convain <thomas.convain@gmail.com>
2024-10-01 00:21:25 +0200

    agrega analytics

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,13 +11,15 @@ const firebaseConfig = {
   projectId: "minutos-87fe9",
   storageBucket: "minutos-87fe9.appspot.com",
   messagingSenderId: "28186647799",
-  appId: "1:28186647799:web:1228819cff4f9d77c987f2"
+  appId: "1:28186647799:web:1228819cff4f9d77c987f2",
+  measurementId: "G-0YK2NL06SG"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, storage };
+export { db, storage, analytics };
