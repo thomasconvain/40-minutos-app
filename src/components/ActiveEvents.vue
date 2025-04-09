119077f81b5b511d7e0b0ab7ad003a45b1071f92
Xavi Rodrigûx <haviert@gmail.com>
2025-04-09 14:36:50 -0400

    agrega u carousel en los eventos activos

--- src/components/ActiveEvents.vue
+++ src/components/ActiveEvents.vue
@@ -1,105 +1,100 @@
 <template>
     <div>
-        <section v-for="event in events" :key="event.id" class="py-8">
-          <div
-            class="mx-auto grid max-w-screen-xl rounded-lg bg-black p-8 md:p-8 lg:grid-cols-12 lg:gap-8 lg:p-16 xl:gap-16 background-circle"
-          >
-            <div class="me-auto place-self-center lg:col-span-7">
-              <h1
-                class="mb-3 text-2xl font-bold leading-tight tracking-tight text-white md:text-4xl"
-              >
-                {{ event.name }}<br />
-                {{ convertTimestamp(event.date) }}
-              </h1>
-              <p v-if="event.isOver" class="mb-6 text-gray-100">
-                Evento terminado. Si aún no realizaste tu aporte puedes hacerlo haciendo click en el siguiente botón.
-              </p>
-              <p v-else-if="event.isFreeEntrance" class="mb-6 text-gray-100">
-                {{ event.place }}
-              </p>
-              <div class="flex flex-wrap gap-3">
-                <button
-                  v-if="!event.isOver && event.isFreeEntrance && !currentUser"
-                  type="button"
-                  class="btn-md mt-2 sm:w-auto w-full inline-flex justify-center items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-black bg-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 disabled:opacity-50 disabled:cursor-not-allowed"
-                  @click="
-                    router.push({ name: 'SignIn', params: { idEvent: event.id } })
-                  "
-                >
-                  <span>Quiero asistir</span>
-                </button>
-                <button
-                  v-if="!event.isOver && event.isFreeEntrance && currentUser && !isSpectatorSubscribed(event.id)"
-                  type="button"
-                  class="btn-md mt-2 sm:w-auto w-full inline-flex justify-center items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-black bg-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 disabled:opacity-50 disabled:cursor-not-allowed"
-                  @click="openModal(event.id, 'modalValidation')"
-                >
-                  <span>Consigue tu ticket en un click</span>
-                </button>
-                <dialog id="modalValidation" class="modal">
-                    <div class="modal-box">
-                        <h3 class="text-lg font-bold">¿Cuantas personas asistirán en total?</h3>
-                        <input v-model="numberOfPeople" type="number" min="1" placeholder="Ingresa el número de participantes" class="input input-bordered w-full mt-4" />
-                        <form method="dialog">
-                            <button
-                                class="btn-md btn btn-primary text-white w-full mt-4"
-                                @click="addSubscribedEvent(currentEventIdForModal)">
-                                Me inscribo
-                            </button>
-                        </form>
+      <section class="py-8">
+        <div class="mx-auto max-w-screen-xl">
+          <h2 class="mb-6 text-2xl font-bold text-black">Eventos disponibles</h2>
+          
+          <!-- Carrusel de daisyUI -->
+          <div class="carousel rounded-box">
+            <!-- Itera sobre cada evento dentro del carrusel -->
+            <div v-for="event in activeEvents" :key="event.id" class="carousel-item w-full md:w-1/2 lg:w-1/3 p-2">
+              <!-- Cada evento como una tarjeta dentro del carrusel -->
+              <div class="card bg-black text-white shadow-xl h-full w-full">
+                <div class="card-body">
+                  <h2 class="card-title text-xl md:text-2xl font-bold">{{ event.name }}</h2>
+                  <p class="text-sm md:text-base">{{ convertTimestamp(event.date) }}</p>
+            
+                  <p v-if="event.isOver" class="my-3 text-gray-300">
+                    Evento terminado. Si aún no realizaste tu aporte puedes hacerlo haciendo click en el siguiente botón.
+                  </p>
+                  <p v-else-if="event.isFreeEntrance" class="my-3 text-gray-300">
+                    {{ event.place }}
+                  </p>
+            
+                  <div class="card-actions mt-4">
+                    <button
+                      v-if="!event.isOver && event.isFreeEntrance && !currentUser"
+                      type="button"
+                      class="btn btn-sm md:btn-md bg-white text-black hover:bg-gray-200 border-transparent"
+                      @click="router.push({ name: 'SignIn', params: { idEvent: event.id } })"
+                    >
+                      <span>Quiero asistir</span>
+                    </button>
+              
+                    <button
+                      v-if="!event.isOver && event.isFreeEntrance && currentUser && !isSpectatorSubscribed(event.id)"
+                      type="button"
+                      class="btn btn-sm md:btn-md bg-white text-black hover:bg-gray-200 border-transparent"
+                      @click="openModal(event.id, 'modalValidation')"
+                    >
+                      <span>Consigue tu ticket en un click</span>
+                    </button>
+              
+                    <div v-if="isSpectatorSubscribed(event.id)" class="flex items-center">
+                      <CheckCircleIcon v-if="!isLoading" class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+                      Ya estas inscrito a este evento
                     </div>
-                    <form method="dialog" class="modal-backdrop">
-                        <button>close</button>
-                    </form>
-                </dialog>
-                <div v-if="isSpectatorSubscribed(event.id)" class="text-white flex items-center">
-                    <CheckCircleIcon v-if="!isLoading" class="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
-                    Ya estas inscrito a este evento</div>
-                <button
-                v-if="!currentUser && event.isOver"
-                  type="button"
-                  class="btn-md mt-2 sm:w-auto w-full inline-flex justify-center items-center px-4 py-2 border border-white shadow-sm text-sm font-medium rounded-md text-white hover:text-black bg-transparent hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 disabled:opacity-50 disabled:cursor-not-allowed"
-                  @click="router.push({ name: 'SignIn' })"
-                >
-                  <span>Realizar mi aporte</span>
-                </button>
+              
+                    <button
+                      v-if="!currentUser && event.isOver"
+                      type="button"
+                      class="btn btn-sm md:btn-md btn-outline text-white hover:bg-white hover:text-black"
+                      @click="router.push({ name: 'SignIn' })"
+                    >
+                      <span>Realizar mi aporte</span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </section>
-        <div
-  class="mx-auto grid max-w-screen-xl rounded-lg bg-white p-8 md:p-8 lg:grid-cols-12 lg:gap-8 lg:p-16 xl:gap-16"
->
-  <div class="me-auto place-self-center lg:col-span-7">
-    <h1
-      class="mb-3 text-2xl font-bold leading-tight tracking-tight text-black md:text-4xl"
-    >
-      ¿Te invitaron a un evento privado?
-    </h1>
-    <p class="mb-6 text-gray-800">
-      Ingresa tu código de acceso acá
-    </p>
-    <div class="flex flex-wrap gap-3">
-      <div class="flex items-center flex-wrap gap-3">
-        <input
-          v-model="codeIdForPrivateEvents"
-          type="text"
-          id="name"
-          placeholder="Ingresa tu código"
-          class="input input-bordered w-full sm:w-auto"
-          required
-        />
-        <button
-          @click="checkIdAndRedirect()"
-          type="button"
-          class="btn-md w-full sm:w-auto inline-flex justify-center items-center px-4 py-2 border border-transparent shadow-sm text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 disabled:opacity-50 disabled:cursor-not-allowed rounded-md"
-        >
-          <span>Confirmar asistencia</span>
-        </button>
+        </div>
+      </section>
+
+      <div class="mx-auto grid max-w-screen-xl rounded-lg bg-white p-8 md:p-8 lg:grid-cols-12 lg:gap-8 lg:p-16 xl:gap-16"
+      >
+        <div class="me-auto place-self-center lg:col-span-7">
+          <h1
+          class="mb-3 text-2xl font-bold leading-tight tracking-tight text-black md:text-4xl"
+          >
+          ¿Te invitaron a un evento privado?
+          </h1>
+          <p class="mb-6 text-gray-800">
+            Ingresa tu código de acceso acá
+          </p>
+          <div class="flex flex-wrap gap-3">
+            <div class="flex items-center flex-wrap gap-3">
+              <input
+                v-model="codeIdForPrivateEvents"
+                type="text"
+                id="name"
+                placeholder="Ingresa tu código"
+                class="input input-bordered w-full sm:w-auto"
+                required
+              />
+              <button
+                @click="checkIdAndRedirect()"
+                type="button"
+                class="btn-md w-full sm:w-auto inline-flex justify-center items-center px-4 py-2 border border-transparent shadow-sm text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 disabled:opacity-50 disabled:cursor-not-allowed rounded-md"
+              >
+                <span>Confirmar asistencia</span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>          <dialog id="modalSuccess" class="modal">
+
+    <dialog id="modalSuccess" class="modal">
             <div class="modal-box">
               <h3 class="text-lg font-bold">¡Muchas gracias!</h3>
               <p class="py-4">Tu inscripción ha sido exitosa. En esta pantalla encontrarás todos los detalles del evento.</p>
@@ -109,13 +104,31 @@
                 </form>
               </div>
             </div>
-          </dialog>
-    </div>
+    </dialog>
+  </div>
+
+  <dialog id="modalValidation" class="modal">
+          <div class="modal-box">
+              <h3 class="text-lg font-bold">¿Cuantas personas asistirán en total?</h3>
+              <input v-model="numberOfPeople" type="number" min="1" placeholder="Ingresa el número de participantes" class="input input-bordered w-full mt-4" />
+              <form method="dialog">
+                  <button
+                      class="btn-md btn btn-primary text-white w-full mt-4"
+                      @click="addSubscribedEvent(currentEventIdForModal)">
+                      Me inscribo
+                  </button>
+              </form>
+          </div>
+          <form method="dialog" class="modal-backdrop">
+              <button>close</button>
+          </form>
+        </dialog>
+        
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
-import { ref, onMounted, defineEmits, defineProps } from "vue";
+import { ref, onMounted, defineEmits, defineProps, computed } from "vue";
 import { collection, getDocs, query, where, orderBy } from "firebase/firestore";
 import { auth } from '@/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
@@ -123,7 +136,6 @@ import { db } from "@/firebase";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
 import { CheckCircleIcon } from '@heroicons/vue/24/outline'
-
 
 const props = defineProps({
   isSpectatorSubscribed: {
@@ -149,6 +161,11 @@ const modalValidation = ref(null);
 
 const currentEventIdForModal = ref(null);
 const codeIdForPrivateEvents = ref(null); 
+
+// Añadir computed property para eventos activos
+const activeEvents = computed(() => {
+  return events.value.filter(event => !event.isOver && event.isFreeEntrance);
+});
 
 const fetchActiveEvents = async () => {
   try {
